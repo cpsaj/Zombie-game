5eb34c0a3674ce0d3fac615aa251333992ed68e6
JosefineM22R <josefine.monrad@gmail.com>
2023-08-28 15:30:29 +0200

    Camera movement
    Rotation så man kan se rundt

--- Zombie/sketch.js
+++ Zombie/sketch.js
@@ -1,6 +1,17 @@
 let sandTexture;
 let skyTexture;
 
+xPos = 0;
+yPos = 0;
+zPos = 0;
+xFocus = 0;
+yFocus = 0;
+zFocus = 1;
+speed = 3;
+rotSpeed = 1;
+vinkel = 90;
+vinkelOp = 0;
+
 function preload()
 {
   sandTexture = loadImage("stylizedSand.jpg");
@@ -18,6 +29,12 @@ function draw()
   background(0,0,255);
   ambientLight(255);
 
+  //Camera
+  camera(xPos, yPos, zPos, //Position
+         xPos + xFocus, yPos + yFocus, zPos + zFocus, //Focuspoint
+         0, 1, 0); //UpVector
+
+  //Sky
   texture(skyTexture);
   createSphere(1000,0,0,0);
 
@@ -25,6 +42,34 @@ function draw()
   noStroke(); 
   texture(sandTexture);
   createPlane(1000, 0, 100, 0, 90, 0, 0);
+
+  //Camera rotation
+  if(keyIsDown(RIGHT_ARROW))
+  {
+    xFocus = cos(vinkel) - (cos(vinkel)*(1-cos(vinkelOp)));
+    zFocus = sin(vinkel) - (sin(vinkel)*(1-cos(vinkelOp)));
+    vinkel += rotSpeed;
+  }
+  if(keyIsDown(LEFT_ARROW))
+  {
+    xFocus = cos(vinkel) - (cos(vinkel)*(1-cos(vinkelOp)));
+    zFocus = sin(vinkel) - (sin(vinkel)*(1-cos(vinkelOp)));
+    vinkel -= rotSpeed;
+  }
+  if(keyIsDown(UP_ARROW))
+  {
+    xFocus = cos(vinkel) - (cos(vinkel)*(1-cos(vinkelOp)));
+    zFocus = sin(vinkel) - (sin(vinkel)*(1-cos(vinkelOp)));
+    yFocus = sin(vinkelOp);
+    vinkelOp -= rotSpeed;
+  }
+  if(keyIsDown(DOWN_ARROW))
+  {
+    xFocus = cos(vinkel) - (cos(vinkel)*(1-cos(vinkelOp)));
+    zFocus = sin(vinkel) - (sin(vinkel)*(1-cos(vinkelOp)));
+    yFocus = sin(vinkelOp);
+    vinkelOp += rotSpeed;
+  }
 }
 
 function createPlane(s, x, y, z, rotX, rotY, rotZ)
